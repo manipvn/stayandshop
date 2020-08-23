14341baff3f3a4abe72f08433c35017ff8e5b837
Pavana Kalluru <pavana.kalluru@oracle.com>
2020-08-23 11:02:59 +0530

    Stay and Shop

---
+++ src/main/java/com/stayandshop/StayandshopApplication.java
@@ -0,0 +1,13 @@
+package com.stayandshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayandshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayandshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stayandshop/StayandshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayandshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayandshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
